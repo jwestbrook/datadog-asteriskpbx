19675c2aa20a1ab355ce104e5a705fd85b723655
Miguel Torres <mafairnet@gmail.com>
2020-04-20 19:18:32 -0500

    Fix: SIP Usage Association per trunks
    SIP Usage Association fixed per trunks

--- asteriskpbx_sample.py
+++ asteriskpbx_sample.py
@@ -221,7 +221,7 @@ for currentChannel in currentChannelsArray:
         sip_trunk_name = sip_trunk_name.strip()
         if sip_trunk_name in currentChannel.Channel:
             channels_in_use = sip_trunks_data[sip_trunk_index]['channels_in_use'] + 1
-            new_data = {sip_trunk_index:{"name":sip_trunk["name"],"type":sip_trunks_data[sip_trunk_index]["type"],"channels":channels_in_use}}
+            new_data = {sip_trunk_index:{"name":sip_trunks_data[sip_trunk_index]["name"],"type":sip_trunks_data[sip_trunk_index]["type"],"channels":channels_in_use}}
             sip_trunks_data.update(new_data)
 
 print("SIP Trunks Channel Usage")
